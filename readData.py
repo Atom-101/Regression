e4e3832b538f458007b7db40e85b11c97c75a163
Atmadeep Banerjee <atmadeepb@gmail.com>
2018-08-18 10:49:16 +0530

    Update readData.py

--- readData.py
+++ readData.py
@@ -3,5 +3,13 @@ import numpy as np
 
 def readData(file):
   df = pd.read_csv(file)
+  for column in df:
+    if (df[column].dtype != np.number):
+      df[column] = pd.factorize(df[column])[0]
+  
+  dataset = df.values
+  
+  return dataset
+
   
   
